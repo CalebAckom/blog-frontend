b5021597a6b54ed1d4c88fd767a16f5ce8096764
CalebAckom <calebackom@amalitech.com>
2022-12-24 11:18:57 +0000

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import HomePage from "./pages/HomePage"
+import SinglePost from "./pages/SinglePost"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <LandingPage/>,
+    children: [
+      {
+        path:"/",
+        element:<HomePage/>,
+      },
+      {
+        path:"/post/:id",
+        element:<SinglePost/>
+      }
+    ]
   },
   {
     path: "/register",
@@ -19,10 +43,13 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
     </div>
   );
 }
+
 
 export default App;
