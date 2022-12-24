68de57aaaff733123792a3ca5052c7aa1ceec142
CalebAckom <calebackom@amalitech.com>
2022-12-24 14:16:08 +0000

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Logo from "./../assets/21.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    {/* eslint-disable-next-line */}
+      <img src={Logo}></img>
+      <span>
+      &#169; 2022 <b>JaCarl Inc</b>
+      </span>
+    </footer>
   )
 }
 
